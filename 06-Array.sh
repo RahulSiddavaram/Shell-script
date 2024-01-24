7dc05365470ac264aa7867d1bac589080cb4c454
RahulSiddavaram <srjk7572@gmail.com>
2024-01-24 12:03:12 +0530

    USERNAME PROMT

--- 06-Array.sh
+++ 06-Array.sh
@@ -3,5 +3,7 @@
 # This is called ARRAY, Instead of single value it can hold multple values
 PERSONS=("Ramesh" "Suresh" "Rahul")
 
+
 echo "First Person: ${PERSONS[0]}"
 
+echo "ALL The Persons in A GROUP: ${PERSONS[@]}"
