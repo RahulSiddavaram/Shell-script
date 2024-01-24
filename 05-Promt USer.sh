01217c316e0a212db5cc3241d06c920c4df0e5ad
RahulSiddavaram <srjk7572@gmail.com>
2024-01-24 11:41:02 +0530

    USERNAME PROMT

--- 05-Promt USer.sh
+++ 05-Promt USer.sh
@@ -5,3 +5,9 @@ echo "please enter ur username"
 read USERNAME # THis is the variable where the value of the one user entered
 
 echo "USername entered is : $USERNAME"
+
+echo "please enter ur Password"
+
+read PASSWORD
+
+echo "Password entered is : $PASSWORD"
