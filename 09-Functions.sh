9f6cee3d0bb69ebcddf612b4a3a4a397b5a144d4
RahulSiddavaram <srjk7572@gmail.com>
2024-01-24 17:10:30 +0530

    conditions script added

--- 09-Functions.sh
+++ 09-Functions.sh
@@ -9,37 +9,43 @@
 # root user id -u --> 0
 # normal user id -u --> not 0
 
-USERID=$(id -u)
+DATE=$(date +&F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
 
 VALIDATE (){
 
 #$1 --> it will receive the argument1
 #$1 --> it will receive the argument2
 
-if [ $1 -ne 0 ]
-then 
-    echo "$1 ERROR"
-    exit 1
-else 
-    echo "$2 SuCCCESS"
-fi
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 ERROR"
+        exit 1
+    else 
+        echo "$2 SUCCCESS"
+    fi
 
 }
 
+USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
-then 
-    echo "ERROR :: PLEASE RUN WITH ROOT ACCESS"
-    exit 1
-# else    
-#     echo "INFO:: YOU ARE ROOT USER"
+    then 
+        echo "ERROR :: PLEASE RUN WITH ROOT ACCESS"
+        exit 1
+    # else    
+    #     echo "INFO:: YOU ARE ROOT USER"
 fi
 
 # it is our responsibility  again to check whether installation is SUccess or not
-yum install mysql -y 
 
-VALIDATE $? "Installing MySQL"
+yum install mysql -y &>>$LOGFILE
 
-yum install postfix -y 
+VALIDATE $? "Installing MySQL" 
+
+yum install postfix -y &>>$LOGFILE
 
 VALIDATE $? "Installing PostFix"
 
