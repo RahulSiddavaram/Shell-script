a0c377ba0bc07df8358f6d869495cb0fb1332324
RahulSiddavaram <srjk7572@gmail.com>
2024-01-24 23:09:46 +0530

    conditions script added

---
+++ 13-DiskUsage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# colour
+# validations
+# log redirections
+
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIRECTORY/$0-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+DISK_USAGE=$(df -hT |grep -vE 'tmpfs|Filesystem')
+DISK_USAGE_THRESHOLD=1
+
+#IFS=means internal field seperator is space.
+WHILE IFS= read line
+do 
+    echo "output: $line"
+    
+done <<< $DISK_USAGE
