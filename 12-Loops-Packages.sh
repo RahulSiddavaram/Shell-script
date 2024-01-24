bb5ffb0ac4440b32107be744b083b44fa05fac70
RahulSiddavaram <srjk7572@gmail.com>
2024-01-24 17:52:35 +0530

    conditions script added

--- 12-Loops-Packages.sh
+++ 12-Loops-Packages.sh
@@ -2,6 +2,25 @@
 
 # all arguments are in $@
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e " $R ERROR :: PLEASE RUN WITH ROOT ACCESS $N "
+    exit 1
+# else    
+#     echo "INFO:: YOU ARE ROOT USER"
+fi
+
+
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m
+
 for i in $@
 do
     yum install $i -y
