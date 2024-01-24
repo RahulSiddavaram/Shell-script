3e9fa4afb3cab1fec211989fe135cb74998d4663
RahulSiddavaram <srjk7572@gmail.com>
2024-01-24 11:30:48 +0530

    aDDITION SCRIPT ADDED

---
+++ 04-Addition.sh
@@ -0,0 +1,9 @@
+#!/bin.bash
+
+#This is the Comment & CLI will not consider this comment line starting with hash
+
+NUMBER1=$1
+NUMBER2=$2
+
+#i NEED TO TRUN A COMMAD TO ADD THE ABOVE NUMBERS
+SUM=$((NUMBER1+NUMBER2))
